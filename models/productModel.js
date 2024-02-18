cf297226980abbeb41cc5704825b39b7edadf68e
Harish Kunwar <kunwarharish11@gmail.com>
2024-02-18 10:21:15 +0545

    Define product schema and model

---
+++ models/productModel.js
@@ -0,0 +1,54 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter product name"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter product description"],
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter product price"],
+    },
+    image:String,
+    category:{
+        type:String,
+        required:[true,"Please enter product category"],
+    },
+    brand:{
+        type:String,
+        required:[true,"Please enter product category"],
+    },
+    quantity:{
+        type:Number,
+        default:0
+    },
+    sold:{
+        type:Number,
+        default:0
+    },
+    color:{
+        type:String,
+        required:true
+    },
+    ratings:[
+        {
+            star:Number,
+            ratedBy:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'User'
+            }
+        }
+    ],
+    avgRate:{
+        type:Number,
+        default:0
+    }
+
+},{timestamps:true});
+
+module.exports=mongoose.model("Product",productSchema);
