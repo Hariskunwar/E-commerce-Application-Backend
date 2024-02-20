5968a898d0931d4524e1beaf2cf5b0ba1b096216
Harish Kunwar <kunwarharish11@gmail.com>
2024-02-20 11:32:41 +0545

    Implement product filtering, sorting,limiting fields and pagination

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,7 +27,54 @@ exports.createProduct=asyncErrorHandler(async (req,res,next)=>{
 
     //get all products
     exports.getAllProduct=asyncErrorHandler(async (req,res,next)=>{
-      const products=await Product.find();
+      
+      //exculde some fields
+      const excludeFields=['sort','page','limit','fields'];
+      let queryObj={...req.query};
+      excludeFields.forEach((el)=>{
+        delete queryObj[el];
+      })
+      
+      //filtering
+      let queryStr=JSON.stringify(queryObj);
+      queryStr=queryStr.replace(/\b(gte|gt|lte|lt)\b/g,(match)=>`$${match}`);
+       queryObj=JSON.parse(queryStr);
+       
+       
+       let query= Product.find(queryObj);
+
+      //sorting
+      if(req.query.sort){
+        const sortedBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortedBy);
+      }else{
+        query=query.sort("-createdAt")
+      }
+
+      //limiting fields
+      if(req.query.fields){
+        const fields=req.query.fields.split(',').join(' ');
+        query=query.select(fields);
+      }else{
+        query=query.select('-__v');
+      }
+
+      //pagination
+      const page=req.query.page*1||1;
+      const limit=req.query.limit*1||3;
+      const skip=(page-1)*limit;
+      query=query.skip(skip).limit(limit);
+      if(req.query.page){
+        const productCount=await Product.countDocuments();
+        if(skip>=productCount){
+          return next(new CustomError('This page is not found',404));
+        }
+      }
+
+      const products=await query;
+
+
+      
       res.status(200).json({
         status:"success",
         data:{
