00acf5c877314a487f24ee866d68511f8595907b
Harish Kunwar <harishkunwar755@gmail.com>
2024-05-14 13:40:40 +0545

    Update dbConnect.js
    Comment Added

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,19 @@
-const mongoose=require("mongoose");
+// Import the mongoose library
+const mongoose = require("mongoose");
 
-async function dbConnect(){
+// Define an asynchronous function named dbConnect
+async function dbConnect() {
     try {
+        // Attempt to connect to MongoDB using the MONGO_URI environment variable
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("database connected");
+        
+        // If connection is successful, log a success message to the console
+        console.log("Database connected");
     } catch (error) {
+        // If an error occurs during connection, log the error to the console
         console.log(error);
     }
 }
 
-module.exports=dbConnect;
+// Export the dbConnect function so it can be used by other parts of the application
+module.exports = dbConnect;
